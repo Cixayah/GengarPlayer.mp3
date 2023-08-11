2381933bc356cebdf17559bc90be8e4dc3d5970b
Cix <gabrielviniciodacosta@gmail.com>
2023-08-11 13:04:20 -0300

    #

--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
-const player = document.querySelector("#player");
-const musicName = document.querySelector("#musicName");
-const playPauseButton = document.querySelector("#playPauseButton");
-const prevButton = document.querySelector("#prevButton");
-const nextButton = document.querySelector("#nextButton");
-const currentTime = document.querySelector("#currentTime");
-const duration = document.querySelector("#duration");
-const progressBar = document.querySelector(".progress-bar");
-const progress = document.querySelector(".progress");
-const progressBarContainer = document.querySelector(".progress-bar-container"); // Elemento que envolve a barra de progresso
+// script.js
+const audio = document.getElementById("player");
+const musicName = document.getElementById("musicName");
+const playPauseButton = document.getElementById("playPauseButton");
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+const currentTime = document.getElementById("currentTime");
+const duration = document.getElementById("duration");
+const progress = document.getElementById("progress");
+const volumeSlider = document.getElementById("volumeSlider");
 
-const textButtonPlay = "<i class='bx bx-caret-right'></i>";
-const textButtonPause = "<i class='bx bx-pause'></i>";
-
-const colors = ['#FF1493', '#00FF00', '#0000FF', '#FFFF00', '#FF4500'];
-let currentIndex = 0;
-
-function changeColor() {
-  document.body.style.backgroundColor = colors[currentIndex];
-  currentIndex = (currentIndex + 1) % colors.length;
-}
-
-setInterval(changeColor, 1000); // Altera a cor a cada segundo
 let index = 0;
-let songs = [
+const songs = [
     {
         src: "./musics/1.mp3",
         name: "Ritmadinha Dançante",
@@ -31,89 +19,70 @@ let songs = [
         src: "./musics/2.mp3",
         name: "Tuca Tuca",
     },
-
-  
+    {
+        src: "./musics/3.mp3",
+        name: "Automotivo Bibi",
+    },
     // ... outras músicas
 ];
 
-// Função para carregar e tocar uma música aleatória
-const loadAndPlayRandomSong = () => {
-    shuffleArray(songs);
-    loadAndPlaySong(0); // Carrega e toca a primeira música do array embaralhado
-};
-const audio = new Audio();
-
-const playPause = () => {
-    if (audio.paused) {
-        audio.play();
-        playPauseButton.innerHTML = textButtonPause;
-    } else {
-        audio.pause();
-        playPauseButton.innerHTML = textButtonPlay;
-    }
-};
-
-const updateTime = () => {
-    const currentMinutes = Math.floor(audio.currentTime / 60);
-    const currentSeconds = Math.floor(audio.currentTime % 60);
-    currentTime.textContent = currentMinutes + ":" + formatZero(currentSeconds);
-
-    const durationFormatted = isNaN(audio.duration) ? 0 : audio.duration;
-    const durationMinutes = Math.floor(durationFormatted / 60);
-    const durationSeconds = Math.floor(durationFormatted % 60);
-    duration.textContent = durationMinutes + ":" + formatZero(durationSeconds);
-
-    const progressWidth = durationFormatted
-        ? (audio.currentTime / durationFormatted) * 100
-        : 0;
-
-    progress.style.width = progressWidth + "%";
-};
-
-playPauseButton.addEventListener("click", playPause);
 audio.addEventListener("timeupdate", () => {
     updateTime();
     updateProgressBar();
 });
 
-const loadAndPlaySong = (songIndex) => {
-    if (songIndex >= 0 && songIndex < songs.length) {
-        index = songIndex;
-        audio.src = songs[index].src;
-        musicName.innerHTML = songs[index].name;
-        playPause();
+const playPause = () => {
+    if (audio.paused) {
+        audio.play();
+        playPauseButton.innerHTML = "<i class='bx bx-pause'></i>";
+    } else {
+        audio.pause();
+        playPauseButton.innerHTML = "<i class='bx bx-caret-right'></i>";
     }
 };
 
-prevButton.addEventListener("click", () => loadAndPlaySong(index - 1));
-nextButton.addEventListener("click", () => loadAndPlaySong(index + 1));
+playPauseButton.addEventListener("click", playPause);
 
-const formatZero = (n) => (n < 10 ? "0" + n : n);
-loadAndPlaySong(0);
+const updateTime = () => {
+    const currentMinutes = Math.floor(audio.currentTime / 60);
+    const currentSeconds = Math.floor(audio.currentTime % 60);
+    currentTime.textContent = `${currentMinutes}:${formatZero(currentSeconds)}`;
 
-const volumeSlider = document.querySelector("#volumeSlider");
-
-const updateVolume = () => {
-    const volume = volumeSlider.value / 100;
-    audio.volume = volume;
+    const durationFormatted = isNaN(audio.duration) ? 0 : audio.duration;
+    const durationMinutes = Math.floor(durationFormatted / 60);
+    const durationSeconds = Math.floor(durationFormatted % 60);
+    duration.textContent = `${durationMinutes}:${formatZero(durationSeconds)}`;
 };
-volumeSlider.addEventListener("input", updateVolume);
-volumeSlider.value = audio.volume * 100;
-volumeSlider.value = 50;
+
+const loadAndPlaySong = (songIndex) => {
+    index = (songIndex + songs.length) % songs.length;
+    const song = songs[index];
+    audio.src = song.src;
+    musicName.textContent = song.name;
+    audio.play();
+    playPauseButton.innerHTML = "<i class='bx bx-pause'></i>";
+};
+
+prevButton.addEventListener("click", () => loadAndPlaySong(index - 1));
+nextButton.addEventListener("click", () => loadAndPlaySong((index + 1) % songs.length));
+
+const formatZero = (n) => (n < 10 ? `0${n}` : n);
+
 const updateProgressBar = () => {
     const progressWidth = (audio.currentTime / audio.duration) * 100;
-    progress.style.width = progressWidth + "%";
+    progress.style.width = `${progressWidth}%`;
 };
 
-progressBarContainer.addEventListener("click", (event) => {
-    const progressBarWidth = progressBarContainer.offsetWidth;
-    const clickPosition = event.clientX - progressBarContainer.getBoundingClientRect().left;
+progress.addEventListener("click", (event) => {
+    const progressBarWidth = progress.offsetWidth;
+    const clickPosition = event.clientX - progress.getBoundingClientRect().left;
     const newTime = (clickPosition / progressBarWidth) * audio.duration;
 
     audio.currentTime = newTime;
 });
+
 audio.addEventListener("ended", () => {
-    loadAndPlaySong(index + 1); // Carrega e toca a próxima música
+    loadAndPlaySong(index + 1);
 });
 
 const shuffleArray = (array) => {
@@ -123,9 +92,37 @@ const shuffleArray = (array) => {
     }
 };
 
+window.addEventListener("load", () => {
+    shuffleArray(songs);
+    loadAndPlaySong(0);
+});
 
+// Efeito de movimento do Gengar
+const gengar = document.getElementById("Gengar");
 
+function moveGengar() {
+    const translateX = Math.random() * 100 - 5;
+    const translateY = Math.random() * 10 - 5;
+    gengar.style.transform = `translate(${translateX}px, ${translateY}px)`;
+}
 
+setInterval(moveGengar, 1000); // Move o Gengar a cada segundo
 
-// Chame a função para carregar e tocar uma música aleatória ao carregar a página
-window.addEventListener("load", loadAndPlayRandomSong);
+// Alteração de cor de fundo
+const colors = ['#FF1493', '#00FF00', '#0000FF', '#FFFF00', '#FF4500'];
+let currentIndex = 0;
+
+function changeBackgroundColor() {
+    const newColor = colors[currentIndex];
+    document.body.style.backgroundColor = newColor;
+
+    currentIndex = (currentIndex + 1) % colors.length;
+}
+
+setInterval(changeBackgroundColor, 1000); // Altera a cor de fundo a cada segundo
+
+// Controle de volume
+volumeSlider.addEventListener("input", () => {
+    const volume = volumeSlider.value / 100;
+    audio.volume = volume;
+});
